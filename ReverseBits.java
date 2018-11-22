038897ae7d288f50c44bfc5e921dbfc2c8fa4165
WallfacerRZD <1048058400@qq.com>
2018-11-22 09:50:09 +0800

    菜哭了o(╥﹏╥)o

---
+++ ReverseBits.java
@@ -0,0 +1,19 @@
+/**
+ * @author WallfacerRZD
+ * @date 2018/11/22 9:28
+ */
+public class ReverseBits {
+    public class Solution {
+        public int reverseBits(int n) {
+            int result = 0;
+            for (int i = 0; i < 32; i++) {
+                result += n & 1;
+                n >>>= 1;
+                if (i < 31) {
+                    result <<= 1;
+                }
+            }
+            return result;
+        }
+    }
+}
